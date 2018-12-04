80285c6282e09af8c9aada6864e8e71baf8118ad
Ricardo Lucas <zurguingportugal@gmail.com>
2018-12-04 15:33:39 +0000

    activity de inicio e activity do menu criadas

--- app/src/main/java/pi/pt/ipb/estig/museumapp/MainActivity.java
+++ app/src/main/java/pi/pt/ipb/estig/museumapp/MainActivity.java
@@ -1,7 +1,9 @@
 package pi.pt.ipb.estig.museumapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Method btnInitClick - This method it's executed when the user clicks on start button and opens the menu activity.
+     * @param view - view.
+     */
+    public void btnInitClick(View view) {
+        this.startActivity(new Intent(MainActivity.this, MenuActivity.class));
     }
 }
